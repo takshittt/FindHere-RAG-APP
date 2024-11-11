c3d6d039da70fc31429130632826f9fd3fc4cb24
Takshit Saini <94343242+takshittt@users.noreply.github.com>
2024-11-11 19:02:47 +0530

    Main streamlit file added

---
+++ streamlit_app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+import os
+
+st.set_page_config(page_title="FindHere", layout="wide")
+
+st.markdown("""
+## Welcome to FINDHERE!
+
+This powerful AI chatbot instantly answers questions related to any PDF document you upload. Powered by Google's Gemini Pro and built with advanced Retrieval-Augmented Generation (RAG) technology, FindHere is designed to provide accurate, context-aware responses tailored to your documents.
+
+### How It Works
+
+Follow these simple steps to interact with the chatbot:
+
+1. **Enter Your API Key**: For privacy and security, Document Genie requires your Google API key to access Gemini Pro AI capabilities. https://makersuite.google.com/app/apikey.
+
+2. **Upload PDFs**: Upload one or multiple PDF files. FindHere will analyze the content, break it down into searchable chunks, and prepare it for question-answering.
+
+3. **Ask a Questions**: Type any question related to the uploaded PDFs, and Document Genie will provide an in-depth, context-specific answer.
+""")
+
+api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    
+    return chain
+
+def user_input(user_question, api_key):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    st.write("Reply: ", response["output_text"])
+
+# Streamlit UI
+
+def main():
+    st.header("AI RAG chatbot")
+
+    user_question = st.text_input("Ask Your Question", key="user_question")
+
+    if user_question and api_key:  # Ensure API key and user question are provided
+        user_input(user_question, api_key)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("Submit", key="process_button") and api_key:  # Check if API key is provided before processing
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks, api_key)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
